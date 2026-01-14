71374dd58d222baee85a364e9d5d97457b6aaa99
Vignesh Divvela <vignesh@Vigneshs-MacBook-Air.local>
2026-01-13 20:14:59 -0500

    fixed spacing

--- A2SL/settings.py
+++ A2SL/settings.py
@@ -104,15 +104,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [    
