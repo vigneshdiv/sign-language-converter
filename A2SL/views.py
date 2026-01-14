1dffeedfea09235cd8832b2d62c22cb0941d106d
Vignesh Divvela <vignesh@Vigneshs-MacBook-Air.local>
2026-01-13 20:12:30 -0500

    cleaned code

--- A2SL/views.py
+++ A2SL/views.py
@@ -15,9 +15,7 @@ def animation_view(request):
 		text = request.POST.get('sen', '')
 		if not text:
 			return render(request,'animation.html',{'words':[],'text':''})
-		#tokenizing the sentence
 		text = text.lower()
-		#tokenizing the sentence
 		words = word_tokenize(text)
 
 		tagged = nltk.pos_tag(words)
@@ -27,14 +25,8 @@ def animation_view(request):
 		tense["past"] = len([word for word in tagged if word[1] in ["VBD", "VBN"]])
 		tense["present_continuous"] = len([word for word in tagged if word[1] in ["VBG"]])
 
-
-
-		#stopwords that will be removed
 		stop_words = set(["mightn't", 're', 'wasn', 'wouldn', 'be', 'has', 'that', 'does', 'shouldn', 'do', "you've",'off', 'for', "didn't", 'm', 'ain', 'haven', "weren't", 'are', "she's", "wasn't", 'its', "haven't", "wouldn't", 'don', 'weren', 's', "you'd", "don't", 'doesn', "hadn't", 'is', 'was', "that'll", "should've", 'a', 'then', 'the', 'mustn', 'i', 'nor', 'as', "it's", "needn't", 'd', 'am', 'have',  'hasn', 'o', "aren't", "you'll", "couldn't", "you're", "mustn't", 'didn', "doesn't", 'll', 'an', 'hadn', 'whom', 'y', "hasn't", 'itself', 'couldn', 'needn', "shan't", 'isn', 'been', 'such', 'shan', "shouldn't", 'aren', 'being', 'were', 'did', 'ma', 't', 'having', 'mightn', 've', "isn't", "won't"])
 
-
-
-		#removing stopwords and applying lemmatizing nlp process to words
 		lr = WordNetLemmatizer()
 		filtered_text = []
 		for w,p in zip(words,tagged):
@@ -47,8 +39,6 @@ def animation_view(request):
 				else:
 					filtered_text.append(lr.lemmatize(w))
 
-
-		#adding the specific word to specify tense
 		words = filtered_text
 		temp=[]
 		for w in words:
@@ -76,16 +66,13 @@ def animation_view(request):
 				temp = temp + words
 				words = temp
 
-
 		filtered_text = []
 		for w in words:
 			path = w + ".mp4"
 			f = finders.find(path)
-			#splitting the word if its animation is not present in database
 			if not f:
 				for c in w:
 					filtered_text.append(c)
-			#otherwise animation of word
 			else:
 				filtered_text.append(w)
 		words = filtered_text;
@@ -96,44 +83,34 @@ def animation_view(request):
 		return render(request,'animation.html')
 
 def reflex_view(request):
-	# Get list of available ASL words from static files
 	BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 	asset_dir = os.path.join(BASE_DIR, 'Asset')
 	videos_dir = os.path.join(BASE_DIR, 'Videos')
 	
 	available_words = []
 	
-	# Check Asset directory (primary source for static files)
 	if os.path.exists(asset_dir):
 		for file in os.listdir(asset_dir):
 			if file.endswith('.mp4'):
 				word = file.replace('.mp4', '')
-				# Verify file is accessible via static files
 				path = word + ".mp4"
 				if finders.find(path):
 					available_words.append(word)
 	
-	# Also check Videos directory and copy to Asset if needed (for development)
-	# In production, all videos should be in Asset directory
 	if os.path.exists(videos_dir):
 		for file in os.listdir(videos_dir):
 			if file.endswith('.mp4'):
 				word = file.replace('.mp4', '')
-				# Check if already in available_words
 				if word not in available_words:
-					# Try to find via static files
 					path = word + ".mp4"
 					if finders.find(path):
 						available_words.append(word)
 	
-	# Filter out single letters and keep only meaningful words
 	meaningful_words = [w for w in available_words if len(w) > 1 and not w.isdigit()]
 	
-	# If no words found, provide some defaults
 	if not meaningful_words:
 		meaningful_words = ['Hello', 'Thank', 'You', 'Good', 'Happy']
 	
-	# Pass as JSON for JavaScript
 	words_json = json.dumps(meaningful_words)
 	
 	return render(request, 'reflex.html', {'words': meaningful_words, 'words_json': words_json})
